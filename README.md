a63ff109cac114e86b7d3608670d1568645153f8
Rick Brainerd <rickbrainerd12@gmail.com>
2017-06-28 13:40:12 -0400

    Fixed all problems

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Michael Baywatch
 
 A catalog of Michael Bay's extensive filmography
+
+## Day 3 Homework
+
+* Store the flicks in an array, as well as in the DOM
+
+### Bonus Credit
+
+* Add a _fav_ button or _like_ button to each list item that changes the appearance of that item (_e.g._ add a background color)
+
+### Mega Bonus Credit
+
+* Add a _remove_ button to each list item
+* Make that button actually work
+
+### Super Mega Bonus Credit
+
+* Make sure that both of those buttons affect the array as well, not just the DOM
+* Make the _fav_ button toggle the _fav_ status on or off
+
+### Super Mega Bonus Credit Hyper Fighting
+* Add buttons to move a flick up and down the list
+* Reorder the array as well
--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 const app = {
-    init: function(selectors){
-        this.max = 0
-        this.list = document.querySelector(selectors.listSelector)
-        document
-            .querySelector(selectors.formSelector)
-            .addEventListener('submit', this.handleSubmit.bind(this))
-    },
+  init(selectors) {
+    this.flicks = []
+    this.max = 0
+    this.list = document.querySelector(selectors.listSelector)
 
-    renderListItem: function(flick) {
-        const item = document.createElement('li')
-        item.textContent = flick.name
-        return item
-    },
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener(
+        'submit', 
+        this.handleSubmit.bind(this)
+      )
+  },
 
-    handleSubmit: function(ev) {
-        ev.preventDefault()
-        const f = ev.target
-        const flick = {
-            id: this.max+1,
-            name: f.flickName.value
-        }
+  renderListItem(flick) {
+    const item = document.createElement('li')
+    item.textContent = flick.name
+    return item
+  },
 
-        const listItem = this.renderListItem(flick)
-        this.list.appendChild(listItem)
-        this.max++
-    },
+  handleSubmit(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const flick = {
+      id: this.max + 1,
+      name: f.flickName.value,
+    }
+
+    const listItem = this.renderListItem(flick)
+    this.list.appendChild(listItem)
+
+    this.max ++
+  },
 }
 
 app.init({
-    formSelector: 'form#flick-form',
-    listSelector: '#flick-list',
+  formSelector: 'form#flick-form',
+  listSelector: '#flick-list',
 })
